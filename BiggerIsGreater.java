531bb6d1db226a154954f1c992a922fc69acfb73
Anushka Chandel <51352909+ChandelAnushka@users.noreply.github.com>
2020-08-30 12:25:34 +0530

    BiggerIsGreater hackerrank problem

---
+++ BiggerIsGreater.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BiggerIsGreater {
+
+    
+    static String biggerIsGreater(String w) {
+        char charArray[] = w.toCharArray();
+        int n = charArray.length;
+        int endIndex = 0;
+
+        // Start from the right most character and find the first character
+        // that is smaller than previous character.
+        for (endIndex = n - 1; endIndex > 0; endIndex--) {
+            if (charArray[endIndex] > charArray[endIndex - 1]) {
+                break;
+            }
+        }
+
+        // If no such char found, then all characters are in descending order
+        // means there cannot be a greater string with same set of characters
+        if (endIndex == 0) {
+            return "no answer";
+        } else {
+            int firstSmallChar = charArray[endIndex - 1], nextSmallChar = endIndex;
+
+            // Find the smallest character on right side of (endIndex - 1)'th
+            // character that is greater than charArray[endIndex - 1]
+            for (int startIndex = endIndex + 1; startIndex < n; startIndex++) {
+                if (charArray[startIndex] > firstSmallChar && charArray[startIndex] < charArray[nextSmallChar]) {
+                    nextSmallChar = startIndex;
+                }
+            }
+
+            //Swap the above found next smallest character with charArray[endIndex - 1]
+            swap(charArray, endIndex - 1, nextSmallChar);
+
+            //Sort the charArray after (endIndex - 1)in ascending order
+            Arrays.sort(charArray, endIndex , n);
+
+        }
+        return new String(charArray);
+    }
+
+
+    static void swap(char charArray[], int i, int j) {
+        char temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
+            String in = sc.next();
+            System.out.println(biggerIsGreater(in));
+        }
+    }
+}
